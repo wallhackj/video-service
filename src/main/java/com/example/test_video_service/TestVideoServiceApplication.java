fade042c4f4ffc58d53ca94b5d421e26cb914b34
omnissiah <victorhaideu@gmail.com>
2025-03-06 12:31:03 +0200

    docker-compose

---
+++ src/main/java/com/example/test_video_service/TestVideoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_video_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVideoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestVideoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_video_service/TestVideoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_video_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestVideoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
