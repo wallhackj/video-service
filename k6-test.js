564a79b20d2a96f51b80eab1540f9f21f4e523ce
omnissiah <victorhaideu@gmail.com>
2025-03-22 21:14:18 +0200

    k6 test

--- k6-test.js
+++ k6-test.js
@@ -2,12 +2,30 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-    vus: 10,
-    duration: '3s',
+    startTime: '30s',
+
+    stages: [
+        { duration: '30s', target: 50 },
+        { duration: '30s', target: 50 },
+        { duration: '30s', target: 100 },
+        { duration: '30s', target: 100 }
+        { duration: '30s', target: 250 }
+        { duration: '30s', target: 250 }
+        { duration: '30s', target: 500 }
+        { duration: '30s', target: 500 }
+        { duration: '30s', target: 1000 }
+    ],
+
+    noConnectionReuse: true,
+    userAgent: 'k6_stress_test/1.0'
 };
 
 export default function () {
     let res = http.get('http://app:8080/files');
-    check(res, { 'status was 200': (r) => r.status == 200 });
-    sleep(1);
+        check(res, {
+        'Status 200': (r) => r.status === 200,
+        'Timp răspuns < 500ms': (r) => r.timings.duration < 500
+    });
+
+    sleep(30);
 }
